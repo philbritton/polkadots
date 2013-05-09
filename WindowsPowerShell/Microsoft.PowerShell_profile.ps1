efc0a5c09c254fbf4be6db60c1279dbd2d95fb07
philbritton <phil.m.britton@gmail.com>
2013-05-09 16:37:42 -0400

    added powershell scripts and modules

---
+++ WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,12 @@
+﻿
+# Load posh-git example profile
+. 'C:\Users\pbritton\git\posh-git\profile.example.ps1'
+
+
+# Load posh-git example profile
+. 'C:\Users\pbritton\Documents\WindowsPowerShell\Modules\posh-git\profile.example.ps1'
+
+
+# Load posh-npm example profile
+. 'C:\Users\pbritton\Documents\WindowsPowerShell\Modules\posh-npm\profile.example.ps1'
+
